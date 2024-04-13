a5ad19f4065be6c80ea4e9a824ff281e8b20455a
yoomiky <miky.flores1234@icloud.com>
2024-04-13 09:30:24 -0400

    created checkwin, guessLetter

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame as pg
 import random
 pg.init()
 
-screen = pg.display.set_mode((1080, 500))
+screen = pg.display.set_mode((800, 500))
 pg.display.set_caption("Hangman")
 
 images = []
@@ -21,26 +21,41 @@ def importImages():
         img = pg.image.load(f'images/hangman{i}.png')
         images.append(img)
 
+guessedLetterCounter = 0
 
 def drawHangman():
     
     screen.blit(images[0], (50, 85))
 
-    if guessedLetters == 1:
+    if guessedLetterCounter == 1:
         screen.blit(images[1], (50, 85))
-    elif guessedLetters == 2:
+    elif guessedLetterCounter == 2:
         screen.blit(images[2], (50, 85))
-    elif guessedLetters == 3:
+    elif guessedLetterCounter == 3:
         screen.blit(images[3], (50, 85))
-    elif guessedLetters == 4:
+    elif guessedLetterCounter == 4:
         screen.blit(images[4], (50, 85))
-    elif guessedLetters == 5:
+    elif guessedLetterCounter == 5:
         screen.blit(images[5], (50, 85))
-    elif guessedLetters == 6:
+    elif guessedLetterCounter == 6:
         screen.blit(images[6], (50, 85))
     
-#def checkwin()
-    
+def checkWin():
+    if guessedLetterCounter > 6:
+        print("Congratulations! You failed to guess the word.")
+    elif guessedLetterCounter < 6 and randomChoice:
+        print("Congratulations! You successfully guessed the word", randomChoice, ".")
+
+def guessLetter():
+    while not checkWin():
+        ask = input("Guess a letter: ").lower()
+        if ask in guessedLetters:
+            print("You already guessed that letter.")
+        else:
+            guessedLetters.append(ask)
+            if ask not in randomChoice:
+                guessedLetterCounter += 1
+                print("Incorrect guess!")
 
 
 run = True
@@ -58,7 +73,7 @@ while run:
 
     importImages()
     drawHangman()
-    #create the checkWin()
+    guessLetter()
 
     pg.display.update()
 pg.quit()
